098715719e55f224785985773e3d9af2d397a4a5
zhentaogu <438766114@qq.com>
2019-10-28 09:01:02 +0800

    feat: 添加demo

---
+++ demo/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserHistory } from 'history';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
